8a7dc853ec51fb6ed24cf1c8fa1645ac146bb5d6
Ximena Garcia <ximenag@exo.com.ar>
2016-12-23 11:55:57 -0300

    LINK-113 - E2E tests

---
+++ e2e/jenkins-test-conf.js
@@ -0,0 +1,24 @@
+exports.config = {
+    allScriptsTimeout: 11000,
+
+    specs: [
+        '*.spec.js'
+    ],
+
+    multiCapabilities: [
+        {
+            'browserName': 'phantomjs',
+            'phantomjs.binary.path': require('phantomjs').path,
+            'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
+        }
+    ],
+
+    baseUrl: 'http://127.0.0.1:4000',
+
+    mochaOpts:{
+        reporter: 'xunit-file'
+    },
+
+    framework: 'mocha'
+};
+
