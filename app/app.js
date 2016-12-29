1d15814bb3ba23e9a21750e8a9afc16812ea6717
Ximena Garcia <ximenag@exo.com.ar>
2016-12-29 18:10:21 -0300

    LINK-113 - base working template

--- app/app.js
+++ app/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-(function() {
-    if (!angular.mock) {
-        angular.element(document).ready(function() {
-            angular.bootstrap(document, ['protractor-example']);
-        });
-    }
-    angular.module('protractor-example', []);
-})();
+angular.module('eventolAuth', [
+    'ngMaterial',
+    'ngRoute',
+    'ngMessages',
+    'ngSanitize',
+    'home'
+]);
---
+++ app/appConfig.js
@@ -0,0 +1,21 @@
+'use strict';
+angular.module('eventolAuth')
+    .config(function($mdThemingProvider, $routeProvider, $mdIconProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'src/home/home.html',
+            controller: 'HomeController',
+            controllerAs:'cntrl'
+        })
+        .when('/login', {
+            templateUrl: 'src/login/login.html',
+            controller: 'LoginController as cntrl',
+            controllerAs:'cntrl'
+        })
+        .when('/register', {
+            templateUrl: 'src/register/register.html',
+            controller: 'RegisterController',
+            controllerAs:'cntrl'
+        });
+
+        $mdIconProvider.iconSet('avatars', 'assets/angular-material-assets/icons//avatar-icons.svg', 128);
+    });
