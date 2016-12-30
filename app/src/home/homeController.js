8d22e765243d79f993ee632697868b4fd525e14a
Ximena Garcia <ximenag@exo.com.ar>
2016-12-30 11:22:30 -0300

    LINK-113 - Responsive sidebar

--- app/src/home/homeController.js
+++ app/src/home/homeController.js
@@ -1,5 +1,16 @@
 'use strict';
 
+
 angular.module('home', [])
-    .controller('HomeController', function() {
+    .controller('HomeController', function($scope, $mdSidenav, $mdUtil) {
+
+        function buildToggler(navID) {
+            var debounceFn = $mdUtil.debounce(function() {
+                $mdSidenav(navID)
+                    .toggle();
+            }, 100);
+            return debounceFn;
+        }
+
+        $scope.toggleLeft = buildToggler('left');
     });
